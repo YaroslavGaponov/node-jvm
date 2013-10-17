384e6e9c19260a55a6a97f18b02ff76843f038ad
Yaroslav Gaponov <root@localhost.localdomain>
2013-10-17 04:38:07 -0400

    prepared to next step

--- examples/PrintMessage/src/Application.java
+++ examples/PrintMessage/src/Application.java
@@ -2,9 +2,10 @@
 public class Application {
 
 	public static void main(String[] args) {
+		
 		if (args.length == 0) {
-			System.out.print("java Application {Number}");
-			System.exit(0);
+			System.out.print("help: java Application {Number}");
+			System.exit(-1);
 		}
 		
 		int N = Integer.parseInt(args[0]);
--- libs/jvm.js
+++ libs/jvm.js
@@ -6,6 +6,7 @@ var Opcodes = require("./opcodes.js");
 var Helper = require("./util/helper.js");
 var ClassArea = require("./classfile/classarea.js");
 var Frame = require("./frame.js");
+var Signature = require("./classfile/signature.js");
 var TAGS = require("./classfile/tags.js");
 var ACCESS_FLAGS = require("./classfile/accessflags.js");
 
@@ -65,12 +66,14 @@ JVM.prototype.run = function() {
         switch(cmd) {
             case Opcodes.nop:
                 break;
+            
             case Opcodes.getstatic:
                 var staticField = frame.getConstant(frame.read16());                
                 var packageName = frame.getConstant(frame.getConstant(staticField.class_index).name_index).bytes;
                 var className = frame.getConstant(frame.getConstant(staticField.name_and_type_index).name_index).bytes;                
                 frame.STACK.push(require(util.format("%s/%s/%s.js", __dirname, packageName, className)));
                 break;
+            
             case Opcodes.ldc:
                 var constant = frame.getConstant(frame.read8());
                 switch(constant.tag) {
@@ -81,210 +84,214 @@ JVM.prototype.run = function() {
                         throw new Error("not support constant type");
                 }
                 break;
+            
             case Opcodes.invokestatic:
                 var staticMethod = frame.getConstant(frame.read16());
                 var packageClassName = frame.getConstant(frame.getConstant(staticMethod.class_index).name_index).bytes;
                 var method = frame.getConstant(frame.getConstant(staticMethod.name_and_type_index).name_index).bytes;                
-                var argsType = frame.getConstant(frame.getConstant(staticMethod.name_and_type_index).signature_index).bytes;
+                var argsType = Signature.parse(frame.getConstant(frame.getConstant(staticMethod.name_and_type_index).signature_index).bytes);
                 
                 args = [];
-                switch(argsType) {                    
-                    case "(Ljava/lang/String;)I":                        
-                        args.push(frame.STACK.pop());
-                        break;
-                    case "(I)V":
-                        args.push(frame.STACK.pop());
-                        break;
-                    case "(I)Ljava/lang/Integer;":
-                        args.push(frame.STACK.pop());
-                        break;
-                    default:
-                        throw new Error("Not support type " + argsType);
+                for (var i=0; i<argsType.IN.length; i++) {
+                    args.push(frame.STACK.pop());
                 }
+
                 var ctor = require(util.format("%s/%s.js", __dirname, packageClassName));
-                var result = ctor[method].apply(null, args);
-                if (result) {
-                    frame.STACK.push(result);
+                var res = ctor[method].apply(null, args.reverse());
+                if (argsType.OUT.length != 0) {
+                    frame.STACK.push(res);
                 }
                 break;
+            
             case Opcodes.invokevirtual:
                 var methodRefIndex = frame.read16();
                 var methodName = frame.getConstant(frame.getConstant(frame.getConstant(methodRefIndex).name_and_type_index).name_index).bytes;
-                var argsType = frame.getConstant(frame.getConstant(frame.getConstant(methodRefIndex).name_and_type_index).signature_index).bytes;
+                var argsType = Signature.parse(frame.getConstant(frame.getConstant(frame.getConstant(methodRefIndex).name_and_type_index).signature_index).bytes);
                 
                 var args = [];
-                switch (argsType) {
-                    case "()V":
-                        break;
-                    case "(I)V":
-                    case "(Ljava/lang/String;)V":
-                        args.push(frame.STACK.pop());
-                        break;
-                    case "(Ljava/lang/String;[Ljava/lang/Object;)Ljava/io/PrintStream;":
-                        args.push(frame.STACK.pop());
-                        args.push(frame.STACK.pop());
-                        args = args.reverse();
-                        break;
-                    case "(I)Ljava/lang/StringBuilder;":                        
-                        args.push(frame.STACK.pop());
-                        break;
-                    case "()Ljava/lang/String;":
-                        break;
-                    case "(Ljava/lang/String;)Ljava/lang/StringBuilder;":
-                        args.push(frame.STACK.pop());
-                        break;
-                    default:
-                        throw new Error("Not support type " + argsType);                    
+                for (var i=0; i<argsType.IN.length; i++) {
+                    args.push(frame.STACK.pop());
                 }
+                
                 var obj = frame.STACK.pop();
-                var result = obj[methodName].apply(obj, args);
-                if (result) {
-                    frame.STACK.push(result);
+                var res = obj[methodName].apply(obj, args.reverse());
+                if (argsType.OUT.length != 0) {
+                    frame.STACK.push(res);
                 }
                 break;
+            
             case Opcodes.new:
                 var className = frame.getConstant(frame.getConstant(frame.read16()).name_index).bytes;
                 var ctor = require(util.format("%s/%s.js", __dirname, className));
                 frame.STACK.push(new ctor());
                 break;
+            
             case Opcodes.invokespecial:
                 var methodRefIndex = frame.read16();
                 var methodName = frame.getConstant(frame.getConstant(frame.getConstant(methodRefIndex).name_and_type_index).name_index).bytes;
-                var argsType = frame.getConstant(frame.getConstant(frame.getConstant(methodRefIndex).name_and_type_index).signature_index).bytes;
+                var argsType = Signature.parse(frame.getConstant(frame.getConstant(frame.getConstant(methodRefIndex).name_and_type_index).signature_index).bytes);
+                
                 var args = [];
-                switch (argsType) {
-                    case "(Ljava/lang/String;)V":
-                        args.push(frame.STACK.pop());
-                        break;                    
-                    default:
-                        throw new Error("Not support type " + argsType);
-                    
+                for (var i=0; i<argsType.IN.length; i++) {
+                    args.push(frame.STACK.pop());
                 }
+                
                 var obj = frame.STACK.pop();
-                var result = obj[methodName].apply(obj, args);
-                if (result) {
-                    frame.STACK.push(result);
+                var res = obj[methodName].apply(obj, args.reverse());
+                if (argsType.OUT.length != 0) {
+                    frame.STACK.push(res);
                 }
                 break;
+            
             case Opcodes.iconst_0:
                 frame.STACK.push(0);
                 break;
+            
             case Opcodes.iconst_1:
                 frame.STACK.push(1);
                 break;
+            
             case Opcodes.iconst_2:
                 frame.STACK.push(2);
                 break;
+            
             case Opcodes.iconst_3:
                 frame.STACK.push(4);
                 break;
+            
             case Opcodes.iconst_4:
                 frame.STACK.push(4);
                 break;
+            
             case Opcodes.iconst_5:
                 frame.STACK.push(5);
                 break;
             
            case Opcodes.istore:
                 frame.LOCALS[frame.read8()] = frame.STACK.pop();
-                break;                        
+                break;
+            
            case Opcodes.istore_0:
                 frame.LOCALS[0] = frame.STACK.pop();
-                break;            
+                break;
+            
             case Opcodes.istore_1:
                 frame.LOCALS[1] = frame.STACK.pop();
                 break;
+            
             case Opcodes.istore_2:
                 frame.LOCALS[2] = frame.STACK.pop();
                 break;
+            
             case Opcodes.istore_3:
                 frame.LOCALS[3] = frame.STACK.pop();
                 break;
+            
             case Opcodes.aload_0:
                 frame.STACK.push(frame.LOCALS[0]);
                 break;
+            
             case Opcodes.aload_1:
                 frame.STACK.push(frame.LOCALS[1]);
                 break;
+            
             case Opcodes.aload_2:
                 frame.STACK.push(frame.LOCALS[2]);
                 break;
+            
             case Opcodes.aload_3:
                 frame.STACK.push(frame.LOCALS[3]);
-                break;            
+                break;
+            
             case Opcodes.iload:
                 frame.STACK.push(frame.LOCALS[frame.read8()]);
-                break;                        
+                break;
+            
             case Opcodes.iload_0:
                 frame.STACK.push(frame.LOCALS[0]);
-                break;            
+                break;
+            
             case Opcodes.iload_1:
                 frame.STACK.push(frame.LOCALS[1]);
                 break;
+            
             case Opcodes.iload_2:
                 frame.STACK.push(frame.LOCALS[2]);
                 break;
+            
             case Opcodes.iload_3:
                 frame.STACK.push(frame.LOCALS[3]);
                 break;
 
             case Opcodes.lconst_0:
                 frame.STACK.push(0);
-                break;            
+                break;
+            
             case Opcodes.lconst_1:
                 frame.STACK.push(1);
                 break;
             
             case Opcodes.sipush:
                 frame.STACK.push(frame.read16());
-                break;            
+                break;
+            
             case Opcodes.bipush:
                 frame.STACK.push(frame.read8());
-                break;            
+                break;
+            
             case Opcodes.ifne:
                 var jmp = frame.IP - 1 + Helper.getSInt(frame.read16());                                
                 if (frame.STACK.pop() !== 0) {
                     frame.IP = jmp;
                 }                
                 break;
+            
             case Opcodes.if_icmple:
                 var jmp = frame.IP - 1 + Helper.getSInt(frame.read16());                                
                 if (frame.STACK.pop() >= frame.STACK.pop()) {
                     frame.IP = jmp;
                 }
                 break;
+            
             case Opcodes.if_icmplt:
                 var jmp = frame.IP - 1 + Helper.getSInt(frame.read16());                                
                 if (frame.STACK.pop() > frame.STACK.pop()) {
                     frame.IP = jmp;
                 }
-                break;            
+                break;
+            
             case Opcodes.iinc:
                 frame.LOCALS[frame.read8()] += frame.read8();
                 break;
+            
             case Opcodes.anewarray:
                 var type = frame.read16();
                 var size = frame.STACK.pop();
                 frame.STACK.push(new Array(size));                
                 break;
+            
             case Opcodes.dup:
                 var ref = frame.STACK.pop();
                 frame.STACK.push(ref);
                 frame.STACK.push(ref);
                 break;
+            
             case Opcodes.pop:
                 frame.STACK.pop();
                 break;
+            
             case Opcodes.aastore:
                 var val = frame.STACK.pop();
                 var indx = frame.STACK.pop();                
                 var ref = frame.STACK.pop();                
                 ref[indx] = val;
                 break;
+            
             case Opcodes.arraylength:
                 var ref = frame.STACK.pop();
                 frame.STACK.push(ref.length);
                 break;
+            
             case Opcodes.if_icmpeq:
                 var jmp = frame.IP - 1 + Helper.getSInt(frame.read16());                                
                 var ref1 = frame.STACK.pop();
@@ -293,20 +300,24 @@ JVM.prototype.run = function() {
                     frame.IP = jmp;
                 }
                 break;
+            
             case Opcodes.aaload:
                 var indx = frame.STACK.pop();
                 var ref = frame.STACK.pop();                
                 frame.STACK.push(ref[indx]);
                 break;
+            
             case Opcodes.goto:                
                 frame.IP += Helper.getSInt(frame.read16()) - 1;
-                break;            
+                break;
+            
             case Opcodes.return:
                 if (this.frames.length > 0) {
                     frame = this.frames.pop();
                 } else {
                     process.exit();
                 }
+                
             default:                
                 throw new Error(util.format("Command '%s' [0x%s] is not supported.", Opcodes.toString(cmd), cmd.toString(16)));
         }
