043f0b8a9a1d0a6d46904fa43f2efe9e50ed50dd
parroit <andrea.parodi@ebansoftware.net>
2013-11-01 16:15:54 +0100

    added mocha test and travis integration

---
+++ test/Object_test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var expect = require('chai').expect;
+var fs = require('fs');
+
+
+require('chai').should();
+
+
+describe('Object', function () {
+    describe("module", function () {
+        it("should load", function () {
+            expect(1).to.be.a('number');
+
+        });
+    });
+});
