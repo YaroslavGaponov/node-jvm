23899534038f62300a679e94f59a3b500c6f67c5
parroit <andrea.parodi@ebansoftware.net>
2013-11-01 16:33:20 +0100

    added object tests

---
+++ test/anObject.java
@@ -0,0 +1,24 @@
+public class AnObject {
+    private int a;
+
+    public int getB() {
+        AnObject o;
+
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    private int b;
+
+}
