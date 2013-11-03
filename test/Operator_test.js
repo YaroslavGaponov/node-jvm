da5b4973916561b19ea7965861c5b515b429d223
parroit <andrea.parodi@ebansoftware.net>
2013-11-03 23:16:08 +0100

    changed timeout

--- test/Operator_test.js
+++ test/Operator_test.js
@@ -12,7 +12,7 @@ var jvm = new JVM();
 describe('Operator', function () {
     var o1;
     before(function (done) {
-
+        this.timeout(10000);
         child_process.exec("javac Operators.java", {cwd: __dirname}, function (err) {
             if (err) throw err;
             jvm.loadClassFiles(__dirname);
