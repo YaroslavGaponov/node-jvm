37c54e1177aac72c748f4a54b28bc59257cc5849
YaroslavGaponov <yaroslav.gaponov@gmail.com>
2013-11-08 22:25:59 +0200

    test changed

---
+++ test/simple/Object.java
@@ -0,0 +1,5 @@
+package test.simple;
+
+public class Object {
+    
+}
--- test/simple/simple_test.js
+++ test/simple/simple_test.js
@@ -6,64 +6,57 @@ var child_process = require("child_process");
 require('chai').should();
 
 
-var JVM = require("../index");
+var JVM = require("../../index");
 var jvm = new JVM();
 
 describe('Object', function () {
     before(function(done){
         this.timeout(10000);
-        child_process.exec("javac SomeObject.java",{cwd : __dirname},function(err){
+        child_process.exec("javac Object.java",{cwd : __dirname},function(err){
             if (err) throw err;
             jvm.loadClassFiles(__dirname);
             done();
         });
-
-
     });
 
     describe("equals", function () {
         it("should return true for same instance", function () {
-            var o1=CLASSES.createNewObject("SomeObject");
-            expect(o1.equals(o1)).to.be.true;
-
+            var o = CLASSES.createNewObject("test.simple.Object");
+            expect(o.equals(o)).to.be.true;
         });
 
         it("should return false for different instances", function () {
-            var o1=CLASSES.createNewObject("SomeObject");
-            var o2=CLASSES.createNewObject("SomeObject");
+            var o1 = CLASSES.createNewObject("test.simple.Object");
+            var o2 = CLASSES.createNewObject("test.simple.Object");
             expect(o1.equals(o2)).to.be.false;
-
         });
     });
     
     describe("getClass", function () {
         it("return correct class object", function () {
-            var o1=CLASSES.createNewObject("SomeObject");
-            expect(o1.getClassName()).to.be.equal('SomeObject');
+            var o = CLASSES.createNewObject("test.simple.Object");
+            expect(o.getClassName()).to.be.equal("test.simple.Object");
         });
     });
 
     describe("toString", function () {
         it("return correct class object", function () {
-            var o1=CLASSES.createNewObject("SomeObject");
-            expect(o1.toString()).to.be.equal("SomeObject@"+o1.hashCode().toString(16));
+            var o = CLASSES.createNewObject("test.simple.Object");
+            expect(o.toString()).to.be.equal("test/simple/Object" + '@' + o.hashCode().toString(16));
         });
     });
 
     describe("hashcode", function () {
         it("should return an int", function () {
-            var o1=CLASSES.createNewObject("SomeObject");
-            expect(o1.hashCode()).to.be.a('number');
-
-            expect(o1.hashCode()).to.be.greaterThan(0);
-
+            var o = CLASSES.createNewObject("test.simple.Object");
+            expect(o.hashCode()).to.be.a('number');
+            expect(o.hashCode()).to.be.greaterThan(0);
         });
 
         it("should be different for different instances", function () {
-            var o1=CLASSES.createNewObject("SomeObject");
-            var o2=CLASSES.createNewObject("SomeObject");
+            var o1 = CLASSES.createNewObject("test.simple.Object");
+            var o2 = CLASSES.createNewObject("test.simple.Object");
             expect(o1.hashCode()).not.to.be.equal(o2.hashCode());
         });
-
     });
 });
