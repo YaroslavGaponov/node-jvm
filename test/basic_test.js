72c3b58624371c2491cdde37b412518ed7095cbf
YaroslavGaponov <yaroslav.gaponov@gmail.com>
2013-11-13 23:39:57 +0200

    tests fixed

--- test/basic_test.js
+++ test/basic_test.js
@@ -20,41 +20,41 @@ describe('Object', function () {
 
     describe("equals", function () {
         it("should return true for same instance", function () {
-            var o = CLASSES.createNewObject("_basic/Object");
+            var o = CLASSES.newObject("_basic/Object");
             expect(o.equals(o)).to.be.true;
         });
 
         it("should return false for different instances", function () {
-            var o1 = CLASSES.createNewObject("_basic/Object");
-            var o2 = CLASSES.createNewObject("_basic/Object");
+            var o1 = CLASSES.newObject("_basic/Object");
+            var o2 = CLASSES.newObject("_basic/Object");
             expect(o1.equals(o2)).to.be.false;
         });
     });
     
     describe("getClass", function () {
         it("return correct class object", function () {
-            var o = CLASSES.createNewObject("_basic/Object");
+            var o = CLASSES.newObject("_basic/Object");
             expect(o.getClassName()).to.be.equal("_basic/Object");
         });
     });
 
     describe("toString", function () {
         it("return correct class object", function () {
-            var o = CLASSES.createNewObject("_basic/Object");
+            var o = CLASSES.newObject("_basic/Object");
             expect(o.toString()).to.be.equal("_basic/Object" + '@' + o.hashCode().toString(16));
         });
     });
 
     describe("hashcode", function () {
         it("should return an int", function () {
-            var o = CLASSES.createNewObject("_basic/Object");
+            var o = CLASSES.newObject("_basic/Object");
             expect(o.hashCode()).to.be.a('number');
             expect(o.hashCode()).to.be.greaterThan(0);
         });
 
         it("should be different for different instances", function () {
-            var o1 = CLASSES.createNewObject("_basic/Object");
-            var o2 = CLASSES.createNewObject("_basic/Object");
+            var o1 = CLASSES.newObject("_basic/Object");
+            var o2 = CLASSES.newObject("_basic/Object");
             expect(o1.hashCode()).not.to.be.equal(o2.hashCode());
         });
     });
